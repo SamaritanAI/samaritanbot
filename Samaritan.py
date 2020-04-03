5a2cc7c6fdec3ff9036370a82f65479a1d954d42
SamaritanAI <33179496+SamaritanAI@users.noreply.github.com>
2020-04-03 18:57:48 +0200

    Add files via upload

--- Samaritan.py
+++ Samaritan.py
@@ -1,19 +1,19 @@
+import discord
+from discord.ext import commands
+import aiohttp
 import asyncio
 import os
+import random
 
-bot = commands.Bot(description="This my second bot. A lot of work will be going into it. None of this would be possible without you, Sebi. Thanks bro ^_^... I apologise for many of the errors you'll find here.", command_prefix=("s.", "S.", "samaritan.", "Samaritan."),pm_help=True)
 bot = commands.Bot(description="This my second bot. A lot of work will be going into it. None of this would be possible without you, Sebi. Thanks bro ^_^... I apologise for many of the errors you'll find here.", command_prefix=("s.", "S.", "samaritan.", "Samaritan."), pm_help=True)
 
 @bot.command()
 async def ping(ctx):
-    return await ctx.send('Pong! {0}'.format(round(bot.latency,1000)))
     """Test the bot's latency"""
-    await bot.say("Pong!") 
     return await ctx.send('Pong! {0}'.format(round(bot.latency,1000)))
     
 @bot.command()
 async def say(ctx, *, something):
-    await ctx.send(something)
     """Get the bot to say what you want it to say"""
     await ctx.send(something)
 
@@ -23,18 +23,18 @@ async def ud(ctx, *, term):
     async with aiohttp.request("GET", f"http://api.urbandictionary.com/v0/define?term={term}") as res:
         data = await res.json()
     definition = data["list"][0]
-@ -30,37 +28,34 @@ async def ud(ctx, *, term):
+    message = f"""
+    Word: {definition["word"]}
+    Definition: {definition["definition"]}
     Example: {definition["example"]}
     """
     await ctx.send(message)
-    """Search something on Urban Dictionary"""
 
 @bot.command()
 async def avatar(ctx, *, user:discord.Member = None):
     """Englarge someone's avatar"""
     user = user or ctx.author
     await ctx.send(user.avatar_url)
-    """Englarge someone's avatar"""
 
 @bot.command()
 async def emoji(ctx, *, emoji):
@@ -42,28 +42,33 @@ async def emoji(ctx, *, emoji):
     custom = emoji.split(":")[-1][:-1]
     url = f"https://cdn.discordapp.com/emojis/{custom}.png" 
     await ctx.send(url)
-    """Enlarges emoji"""
 
 helpcmd = bot.get_command("help")
 @helpcmd.after_invoke
 async def help_after(ctx):
     await ctx.send("Help has been DM'd.")
 
-@bot.command()
-async def spam(ctx, count: int, *, input: commands.clean_content):
-    """Sends spam"""
-    for i in range(count):
-        await ctx.send(input)
-        await asyncio.sleep(1)
-        if i == 500:
-            await ctx.send("Limit Reached")
-            break
-        """Sends spam"""
-
-
 @bot.listen('on_ready')
+@bot.listen('on_connect')
 async def get_owner():
+    app_info = await bot.application_info()
     bot.owner_id = app_info.owner.id
 
+
+	
+@bot.command()
+async def flip(ctx):
+		x = random.randint(1,3)
+		if x == 1:
+			x = "Heads"
+		else:
+			x = "Tails"
+		return x
+		
+		await ctx.send(x)
+
+	
+	
 bot.load_extension('libneko.extras.superuser')
 bot.run(os.environ.get("TOKEN"))
+
